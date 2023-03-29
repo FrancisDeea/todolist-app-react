d0149262fe5ecffd5ca4bd9c7969bd8aa2f646a0
Francis <francisbernal14@gmail.com>
2023-03-29 23:12:12 +0200

    feat: Improve style and create useEffect to handle transition effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function App() {
   const [input, setInput] = useState("");
   const [list, setList] = useState([]);
+  const [height, setHeight] = useState(0);
   const [tab, setTab] = useState("");
   const [darkMode, setDarkMode] = useState(false);
+  const storage = localStorage;
 
+  // Add new task when enter key is pressed
   function handleEnter(e) {
     const key = e.key;
     if (key === "Enter" && input) {
@@ -15,8 +19,25 @@ export default function App() {
     }
   }
 
+  // Update height of container when list changes
+  useEffect(() => {
+    let container = document.querySelector(".app-container");
+    setHeight(container.scrollHeight);
+
+    if (container.offsetHeight >= (window.innerHeight * 0.60)) {
+      container.style.overflow = "auto";
+    }
+  }, [list])
+
+
   return (
-    <div className='app-container'>
+    <div
+      className='app-container'
+      style={{
+        height: `${height}px`,
+        transition: `height .2s ease`
+      }}
+    >
       <InputTask
         input={input}
         onChange={(e) => setInput(e.target.value)}
@@ -44,6 +65,7 @@ function InputTask({ input, onChange, onKeyDown }) {
 
 function List({ list }) {
   let key = 0;
+  // Map over list and create a new li element for each task
   const todo = list.map(task => {
     key++;
     return (
